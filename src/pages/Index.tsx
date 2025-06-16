891dc574d6768dc5770837e1ca044ef558896293
RiteshSingh <yashvant.ritesh@gmail.com>
2025-06-16 16:41:17 +0530

    Testing full res images

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -11,11 +11,13 @@ import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@
 const DEEP_FIELD_IMAGES = [
   {
     name: "Webb First Deep Field",
-    url: '/deepfield/webb1024.jpg',
+    //url: '/deepfield/webb1024.jpg',
+    url: '/deepfield/webbs-first-deep-field.jpg'
   },
   {
     name: "Hubble Ultra-Deep Field",
-    url: '/deepfield/hubble1024.jpg',
+    //url: '/deepfield/hubble1024.jpg',
+    url: '/deepfield/hubble-ultra-deep-field.jpg',
   },
 ];
 
